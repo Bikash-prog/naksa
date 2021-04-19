19e1e45dda23a36d21085590cb4648635d2b0e03
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2021-04-19 19:05:35 +0530

    Update appdata.dart

--- lib/data_provider/appdata.dart
+++ lib/data_provider/appdata.dart
@@ -2,11 +2,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:naksa/data_models/address.dart';
-
+// This whole class check and store changes and listen it
 class AppData extends ChangeNotifier
 {
   Address originAddress;
   Address destinationAddress;
+  Address place1 , place2 , place3;
 
   void updateOriginAddress(Address origin)
   {
@@ -19,5 +20,22 @@ class AppData extends ChangeNotifier
     destinationAddress = destination;
     notifyListeners();
   }
+
+  void updatePlaceAddress(Address firstPlace)
+  {
+    place1 = firstPlace;
+    notifyListeners();
+  }
+  void updatePlaceAddress2(Address secondPlace)
+  {
+    place2 = secondPlace;
+    notifyListeners();
+  }
+  void updatePlaceAddress3(Address thirdPlace)
+  {
+    place3 = thirdPlace;
+    notifyListeners();
+  }
+
 
 }
