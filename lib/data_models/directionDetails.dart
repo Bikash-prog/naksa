6218f27bc1a7c86eaca4ef658f3ffb5bc89dbb10
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2021-04-19 19:04:35 +0530

    Update directionDetails.dart

--- lib/data_models/directionDetails.dart
+++ lib/data_models/directionDetails.dart
@@ -1,3 +1,8 @@
+
+// This class gives the direction details
+// Like distance and duration between two points (2 by 2)
+// We have only used distanceText , durationText and encodedPoints in homePage Map
+
 class DirectionDetails{
 
   String distanceText;
@@ -8,4 +13,16 @@ class DirectionDetails{
 
   DirectionDetails({this.distanceText,this.distanceValue,this.durationText,this.durationValue,this.encodedPoints});
 
+}
+
+
+//Not functional till now as we haven't used DistanceMatrix URL
+// It'll be helpful while making 3 by 3 Distance and Time Matrix
+// In this case we can use this DistanceMatrixTimeDistance Class
+
+class DistanceMatrixTimeDistance{
+  String durationText;
+  String distanceText;
+
+  DistanceMatrixTimeDistance({this.durationText,this.distanceText});
 }
