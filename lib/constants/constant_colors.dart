6fc2c0ce87d4c09240d83bffc1c1d88cf5d16447
Ayush Verma <78803462+Verma-Ayush@users.noreply.github.com>
2021-05-09 23:01:14 +0530

    Update constant_colors.dart

--- lib/constants/constant_colors.dart
+++ lib/constants/constant_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+//followings colors are used as base colors of our app
 final Color bodyBlackColor = Color(0xff393e46);
 final Color bodyWhiteColor = Color(0xffffffff);
 final Color buttonColor = Color(0xffec4646);
