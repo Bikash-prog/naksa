b14bffddd8c28b045a7dc5dd9cfe6b9e4190f123
Bikash Bhuyan <bikashbhuyan802@gmail.com>
2021-05-11 19:31:40 +0530

    Update helperMethods.dart

--- lib/helpers/helperMethods.dart
+++ lib/helpers/helperMethods.dart
@@ -65,4 +65,91 @@ class HelperMethods
     return directionDetails;
 
   }
+
+
+
+  static Future<List<DirectionDetails>> getDmDetails() async {
+
+    //BHU
+    LatLng p1 = LatLng(25.2677,82.9913);
+    //VT
+    LatLng p2 = LatLng(25.3109,83.0107);
+    //Varanasi Cantt.
+    LatLng p3 = LatLng(25.3291,82.9658);
+
+
+    String url = 'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${p1.latitude},${p1.longitude}|${p2.latitude},${p2.longitude}|${p3.latitude},${p3.longitude}&destinations=${p1.latitude},${p1.longitude}|${p2.latitude},${p2.longitude}|${p3.latitude},${p3.longitude}&key=$mapApiKey';
+    var response = await RequestHelper.getRequest(url);
+    if(response == 'failed')
+      {
+        return null;
+      }
+    DirectionDetails d1d3= DirectionDetails();
+    DirectionDetails d3d1 = DirectionDetails();
+    DirectionDetails d1d2 = DirectionDetails();
+    DirectionDetails d2d3 = DirectionDetails();
+    DirectionDetails d2d1= DirectionDetails();
+    DirectionDetails d3d2 = DirectionDetails();
+
+
+    d1d2.durationText =  response['rows'][0]['elements'][1]['duration']['text'];
+    d1d2.distanceText = response['rows'][0]['elements'][1]['distance']['text'];
+
+    d1d3.distanceText = response['rows'][0]['elements'][2]['distance']['text'];
+    d1d3.durationText = response['rows'][0]['elements'][2]['duration']['text'];
+
+    d2d3.distanceText = response['rows'][1]['elements'][2]['distance']['text'];
+    d2d3.durationText = response['rows'][1]['elements'][2]['duration']['text'];
+
+    d2d1.distanceText =  response['rows'][1]['elements'][0]['distance']['text'];
+    d2d1.durationText = response['rows'][1]['elements'][0]['duration']['text'];
+
+    d3d1.distanceText = response['rows'][2]['elements'][0]['distance']['text'];
+    d3d1.durationText = response['rows'][2]['elements'][0]['duration']['text'];
+
+    d3d2.distanceText = response['rows'][2]['elements'][1]['distance']['text'];
+    d3d2.durationText = response['rows'][2]['elements'][1]['duration']['text'];
+
+    List<DirectionDetails> values = [d1d2,d1d3,d2d3,d2d1,d3d1,d3d2];
+
+    return values;
+    // return d1d2;
+
+        // List<String> values;
+        // DirectionDetails d11,d12,d13,d21,d22,d23,d31,d32,d33 = DirectionDetails();
+        // d11.distanceText = response['rows'][0]['elements'][0]['distance']['text'];
+        // d11.durationText = response['rows'][0]['elements'][0]['duration']['text'];
+        //
+        // values[0] = d11.distanceText;
+        // values[1] = d11.durationText;
+        // d12.distanceText = response['rows'][0]['elements'][1]['distance']['text'];
+        // d12.durationText = response['rows'][0]['elements'][1]['duration']['text'];
+        // values[2] = d12.distanceText;
+        // values[3] = d12.durationText;
+        // values[4] = d13.distanceText = response['rows'][0]['elements'][2]['distance']['text'];
+        // values[5] = d13.durationText = response['rows'][0]['elements'][2]['duration']['text'];
+        //
+        // values[6] = d21.distanceText = response['rows'][1]['elements'][0]['distance']['text'];
+        // values[7] = d21.durationText = response['rows'][1]['elements'][0]['duration']['text'];
+        //
+        // values[8] = d22.distanceText = response['rows'][1]['elements'][1]['distance']['text'];
+        // values[9] = d22.durationText = response['rows'][1]['elements'][1]['duration']['text'];
+        //
+        // values[10] = d23.distanceText = response['rows'][1]['elements'][2]['distance']['text'];
+        // values[11] = d23.durationText = response['rows'][1]['elements'][2]['duration']['text'];
+        //
+        // values[12] = d31.distanceText = response['rows'][2]['elements'][0]['distance']['text'];
+        // values[13] = d31.durationText = response['rows'][2]['elements'][0]['duration']['text'];
+        //
+        // values[14] = d32.distanceText = response['rows'][2]['elements'][1]['distance']['text'];
+        // values[15] = d32.durationText = response['rows'][2]['elements'][1]['duration']['text'];
+        //
+        // values[16] = d33.distanceText = response['rows'][2]['elements'][2]['distance']['text'];
+        // values[17] = d33.durationText = response['rows'][2]['elements'][2]['duration']['text'];
+
+        //
+        // return d1d2;
+
+
+  }
 }
